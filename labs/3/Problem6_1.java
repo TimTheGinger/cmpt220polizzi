8c7a0237ffd77768fc0ae83d0da362d28fce2528
Tim <timpolizzi2@gmail.com>
2018-02-13 17:04:49 -0500

    Lab 3 update
    updated to google formatting standards.

--- labs/3/Problem6_1.java
+++ labs/3/Problem6_1.java
@@ -1,9 +1,9 @@
 public class Problem6_1 {
 
   public static void main(String[] args) {
-    for (int i = 0; i <= 100; i+=10) {
-      for(int j = 1; j <= 10; j++) {
-        System.out.printf("%7d", getPentagonalNumber(i+j));
+    for (int i = 0; i <= 100; i += 10) {
+      for (int j = 1; j <= 10; j++) {
+        System.out.printf("%7d", getPentagonalNumber(i + j));
       }
       System.out.println();
     }
--- labs/3/Problem6_5.java
+++ labs/3/Problem6_5.java
@@ -10,7 +10,7 @@ public class Problem6_5 {
     int num2 = scan.nextInt();
     int num3 = scan.nextInt();
 
-    displaySortedNumbers(num1,num2,num3);
+    displaySortedNumbers(num1, num2, num3);
   }
 
   public static void displaySortedNumbers(double num1, double num2, double num3) {
