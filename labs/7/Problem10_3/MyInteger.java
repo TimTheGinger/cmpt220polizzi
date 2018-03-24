a97190cd66011f8aa783cb584f7e00670a4f3050
Tim <timpolizzi2@gmail.com>
2018-03-23 23:37:28 -0400

    Lab 7
    All of lab 7 finished

---
+++ labs/7/Problem10_3/MyInteger.java
@@ -0,0 +1,183 @@
+package Problem10_3;
+
+import java.lang.Math;
+
+public class MyInteger {
+
+  //stores the int value represented by MyInteger
+  private int value;
+
+  /**
+   * Basic constructor, sets an integer value to MyInteger
+   *
+   * @param val The integer value of the MyInteger object
+   */
+  public MyInteger(int val) {
+    value = val;
+  }
+
+  /**
+   * Gets the integer value of this MyInteger object
+   *
+   * @return The integer value of this MyInteger object
+   */
+  public int getValue() {
+    int temp = value;
+
+    return temp;
+  }
+
+  /**
+   * Checks if the integer value of this MyInteger is even
+   *
+   * @return True if the integer is even, false otherwise
+   */
+  public boolean isEven() {
+    if (value % 2 == 0) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Checks if the integer value of val is even
+   *
+   * @return True if the number is even, false otherwise
+   */
+  public static boolean isEven(int val) {
+    MyInteger temp = new MyInteger(val);
+
+    return temp.isEven();
+  }
+
+  /**
+   * Checks if the integer value of the MyInteger val is even
+   *
+   * @return True if the number is even, false otherwise
+   */
+  public static boolean isEven(MyInteger val) {
+    return val.isEven();
+  }
+
+  /**
+   * Checks if the integer value of this MyInteger is odd
+   *
+   * @return True if the number is odd, false otherwise
+   */
+  public boolean isOdd() {
+    if (value % 2 != 0) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Checks if the integer value of val is odd
+   *
+   * @return True if the number is odd, false otherwise
+   */
+  public static boolean isOdd(int val) {
+    MyInteger temp = new MyInteger(val);
+
+    return temp.isOdd();
+  }
+
+  /**
+   * Checks if the integer value of the MyInteger val is odd
+   *
+   * @return True if the number is odd, false otherwise
+   */
+  public static boolean isOdd(MyInteger val) {
+    return val.isOdd();
+  }
+
+  /**
+   * Checks if the integer value of this MyInteger is prime
+   *
+   * @return True if the number is prime, false otherwise
+   */
+  public boolean isPrime() {
+    if (value < 2) {
+      return true;
+    }
+
+    if (value % 2 == 0) {
+      return false;
+    }
+
+    int root = (int) Math.sqrt((double) value);
+
+    for (int i = 3; i <= root; i += 2) {
+      if (value % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Checks if the integer value of val is prime
+   *
+   * @return True if the number is prime, false otherwise
+   */
+  public static boolean isPrime(int val) {
+    MyInteger temp = new MyInteger(val);
+
+    return temp.isPrime();
+  }
+
+  /**
+   * Checks if the integer value of the MyInteger val is prime
+   *
+   * @return True if the number is prime, false otherwise
+   */
+  public static boolean isPrime(MyInteger val) {
+    return val.isPrime();
+  }
+
+  /**
+   * Checks if the integer value of this MyInteger is equal to another integer
+   *
+   * @param val The integer to be compared against
+   * @return True if the integer values of val and this MyInteger are equal, false otherwise
+   */
+  public boolean equals(int val) {
+    if (val == value) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Checks if the integer value of this MyInteger is equal to another MyInteger
+   *
+   * @param val The MyInteger to be compared against
+   * @return True if the integer values of val and this MyInteger are equal, false otherwise
+   */
+  public boolean equals(MyInteger val) {
+    if (val.getValue() == value) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Converts an array of char values into an integer
+   *
+   * @param toParse The array of char values
+   * @return the integer representation of toParse
+   */
+  public static int parseInt(char[] toParse) {
+    return Integer.valueOf(String.valueOf(toParse));
+  }
+
+  /**
+   * Converts a String into an integer
+   *
+   * @param toParse The string representation of a integer
+   * @return the integer representation of toParse
+   */
+  public static int parseInt(String toParse) {
+    return Integer.valueOf(toParse);
+  }
+}
---
+++ labs/7/Problem10_3/MyIntegerTester.java
@@ -0,0 +1,93 @@
+package Problem10_3;
+
+import java.util.*;
+
+public class MyIntegerTester {
+
+  public static void main(String[] args) {
+    //get();
+    //evens();
+    //odds();
+    //primes();
+    //equality();
+    //parseChar();
+    //parseString();
+  }
+
+  private static void get() {
+    MyInteger test = new MyInteger(1);
+
+    System.out.println(test.getValue());
+  }
+
+  private static void evens() {
+    MyInteger even = new MyInteger(222);
+    MyInteger odd = new MyInteger(999);
+
+    System.out.println(even.getValue() + " is even: " + even.isEven());
+    System.out.println(odd.getValue() + " is even: " + odd.isEven());
+
+    System.out.println(242 + " is even: " + MyInteger.isEven(242));
+    System.out.println(191 + " is even: " + MyInteger.isEven(191));
+
+    System.out.println("MyInteger even is even: " + MyInteger.isEven(even));
+    System.out.println("MyInteger odd is even: " + MyInteger.isEven(odd));
+  }
+
+  private static void odds() {
+    MyInteger even = new MyInteger(222);
+    MyInteger odd = new MyInteger(999);
+
+    System.out.println(even.getValue() + " is odd: " + even.isOdd());
+    System.out.println(odd.getValue() + " is odd: " + odd.isOdd());
+
+    System.out.println(242 + " is odd: " + MyInteger.isOdd(242));
+    System.out.println(191 + " is odd: " + MyInteger.isOdd(191));
+
+    System.out.println("MyInteger even is odd: " + MyInteger.isOdd(even));
+    System.out.println("MyInteger odd is odd: " + MyInteger.isOdd(odd));
+  }
+
+  private static void primes() {
+    MyInteger prime = new MyInteger(3);
+    MyInteger notPrime = new MyInteger(4);
+
+    System.out.println(prime.getValue() + " is prime: " + prime.isPrime());
+    System.out.println(notPrime.getValue() + " is prime: " + notPrime.isPrime());
+
+    System.out.println(5 + " is prime: " + MyInteger.isPrime(5));
+    System.out.println(6 + " is prime: " + MyInteger.isPrime(6));
+
+    System.out.println("MyInteger prime is prime: " + MyInteger.isPrime(prime));
+    System.out.println("MyInteger not prime is prime: " + MyInteger.isPrime(notPrime));
+  }
+
+  private static void equality() {
+    MyInteger myInt1 = new MyInteger(999);
+
+    System.out.println("999 == myInt1: " + myInt1.equals(999));
+    System.out.println("998 == myInt1: " + myInt1.equals(998));
+
+    MyInteger myInt2 = new MyInteger(999);
+    MyInteger myInt3 = new MyInteger(998);
+
+    System.out.println("myInt2 == myInt1: " + myInt1.equals(myInt2));
+    System.out.println("myInt3 == myInt1: " + myInt1.equals(myInt3));
+  }
+
+  private static void parseChar() {
+    char[] charList = new char[3];
+    charList[0] = '1';
+    charList[1] = '2';
+    charList[2] = '3';
+
+    System.out.println(MyInteger.parseInt(charList));
+  }
+
+  private static void parseString() {
+    String intStr = "123";
+
+    System.out.println("\"123\" = " + MyInteger.parseInt(intStr));
+  }
+
+}
