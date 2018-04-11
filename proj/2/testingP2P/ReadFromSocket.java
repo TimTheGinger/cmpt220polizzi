9c483c6b2ea6c4d11bd20dfda3b57a6dab1f2043
Tim <timpolizzi2@gmail.com>
2018-04-10 20:16:11 -0400

    A lot of project stuff
    P2P works!!! telnet is now unnessecerry (i can't spell)

--- proj/2/testingP2P/ReadFromSocket.java
+++ proj/2/testingP2P/ReadFromSocket.java
@@ -1,31 +1,59 @@
 package testingP2P;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import simpleChatroom.SimpleChatroom;
 
 public class ReadFromSocket implements Runnable {
 
-  Socket sock;
+  private SocketClient father;
+  private Thread w;
+  private Socket connectionToClient;
 
-  public ReadFromSocket(Socket sock) {
-    this.sock = sock;
-
-    Thread r = new Thread(this);
-    r.start();
+  public ReadFromSocket(SocketClient parent, Socket clientConnection) {
+    father = parent;
+    connectionToClient = clientConnection;
+    w = new Thread(this);
+    w.start();
   }
 
+  /**
+   * Collects input from the ip
+   */
   public void run() {
     try {
-      BufferedReader rd = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-      String str;
-      while (!Thread.interrupted()) {
-        str = rd.readLine();
-        System.out.println(str);
-      }
-      rd.close();
-    } catch (java.io.IOException e) {
+      Scanner in = new Scanner(connectionToClient.getInputStream());
 
+      while (!Thread.interrupted()) {
+        try {
+          String finRead = in.nextLine();
+          readMessage(finRead);
+        } catch (java.util.NoSuchElementException e) {
+          readMessage("Error - unknown String");
+          e.printStackTrace();
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
+
+  /**
+   * Reads a message from a port
+   *
+   * @param input The message to be read
+   */
+  private void readMessage(String input) {
+    System.out.println(input);
+  }
+
+  /**
+   * Ends the run method, effectively deleting the reader
+   */
+  public void annihilate() {
+    w.interrupt();
   }
 }
--- proj/2/testingP2P/SocketClient.java
+++ proj/2/testingP2P/SocketClient.java
@@ -2,30 +2,47 @@ package testingP2P;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class SocketClient {
 
-  Socket sock;
-  int port = 8989;
+  private Socket sock;
+  private static int port;
+  private static String ip;
+  private WriteToSocket writer;
+  private ReadFromSocket reader;
+
 
   public static void main(String[] args) {
-    new SocketClient();
+    Scanner scan = new Scanner(System.in);
+
+    //Gimme my IP Address
+    try {
+      System.out.println(InetAddress.getLocalHost().getHostAddress());
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("Enter a ip and a port");
+    ip = scan.next();
+    port = scan.nextInt();
+    SocketClient c = new SocketClient();
   }
 
   public SocketClient() {
     openSocket();
 
     // write to socket
-    new WriteToSocket(sock);
+    writer = new WriteToSocket(this, sock);
 
     // read from socket
-    new ReadFromSocket(sock);
+    reader = new ReadFromSocket(this, sock);
   }
 
   private void openSocket() {
     // open a socket and connect with a timeout limit
     try {
-      InetAddress addr = InetAddress.getLocalHost();
+      InetAddress addr = InetAddress.getByName(ip);
       SocketAddress sockaddr = new InetSocketAddress(addr, port);
       sock = new Socket();
 
@@ -39,5 +56,10 @@ public class SocketClient {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  public void kill() {
+    writer.annihilate();
+    reader.annihilate();
   }
 }
