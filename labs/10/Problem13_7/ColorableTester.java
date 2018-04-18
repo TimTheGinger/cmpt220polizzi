79364dc411df15465a35b23164b480c750c06bff
Tim <timpolizzi2@gmail.com>
2018-04-18 16:06:31 -0400

    Lab 10
    All Updates

---
+++ labs/10/Problem13_7/ColorableTester.java
@@ -0,0 +1,22 @@
+package Problem13_7;
+
+public class ColorableTester {
+
+  public static void main(String[] args) {
+    GeometricObject13_7[] geo = new GeometricObject13_7[5];
+
+    for (int i = 0; i < 5; i++) {
+      geo[i] = new Square(i + 1);
+    }
+
+    for (GeometricObject13_7 sq : geo) {
+      System.out.print(sq.getArea() + " ");
+      if (sq instanceof Colorable) {
+        ((Colorable) sq).howToColor();
+      }
+      System.out.println();
+    }
+
+  }
+
+}
